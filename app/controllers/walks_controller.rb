4fdaaa6c8d114f59d6696fc2f6b7d1cdde4f1b7d
alcodes299 <alskis299@gmail.com>
2018-02-07 19:41:46 -0500

    All crud working but untested. Writting test before merging to master.

--- app/controllers/walks_controller.rb
+++ app/controllers/walks_controller.rb
@@ -2,11 +2,60 @@ class WalksController < ApplicationController
   def index
     @walks= Walk.all
   end
-  def new
-    @user = User.new
+
+  def show
+    @walk = Walk.find(params[:id])
   end
+
+  def new
+    @user = current_user
+    @walk = Walk.new
+  end
+
   def create
-    if current_user
+    @walks = Walk.all
+    @walk=Walk.new(walk_params)
+    if @walk.save
+      flash[:notice] = "Walk added successfully"
+      redirect_to '/walks'
+    else
+      @bob = @walk.errors.full_messages
+      render :new
     end
+  end
+
+  def edit
+    @user = current_user
+    @walk = Walk.find(params[:id])
+  end
+
+
+  def update
+    @user = current_user
+    @walk = Walk.find(params[:id])
+    if @walk.update(walk_params)
+      redirect_to walk_path(@walk)
+      flash[:notice] = 'Walk updated successfully'
+    else
+      flash[:notice] = 'Walk failed to update'
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @walk = Walk.find(params[:id])
+    if @walk.destroy
+      redirect_to '/walks'
+      flash[:notice] = "Walk deleted successfully"
+    else
+      flash[:notice] = 'Wizard deletion failed'
+      render :index
+    end
+  end
+
+  protected
+  def walk_params
+    params.require(:walk).permit(:user_id, :title, :description, :location )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   get '/', to: "homes#index"
   devise_for :users
 
+  resource :users do
+    resources :walks
+  end
   resources :sites
   resources :walks
   resource :users
-  get '*path', to: "homes#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
