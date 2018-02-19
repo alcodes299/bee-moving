9f1186199787ffe27da943d9cff1d612414db0ba
alcodes299 <alskis299@gmail.com>
2018-02-19 18:30:29 -0500

    Style Changes 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(user_name: "heroAdmin", keeper: true, zipcode: 12345, email: "hero@email.com",password: "password",password_confirmation: "password", role: "admin")
