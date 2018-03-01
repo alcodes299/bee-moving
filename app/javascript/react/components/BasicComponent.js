c16925987228c21abbee5a161b73504dbde4aeb3
alcodes299 <alskis299@gmail.com>
2018-02-28 20:23:59 -0500

    working Javascript

--- app/javascript/react/components/BasicComponent.js
+++ app/javascript/react/components/BasicComponent.js
@@ -8,25 +8,7 @@ class Basics extends Component {
       iss_last: []
     }
   }
-  componentDidMount() {
-    fetch('http://api.open-notify.org/iss-now.json')
-    .then(response => {
-      if (response.ok) {
-        return response;
-      } else {
-        let errorMessage = `${response.status} (${response.statusText})`,
-            error = new Error(errorMessage);
-        throw(error);
-      }
-    })
-    .then(response => response.json())
-    .then(body => {
-      
-      this.setState({ iss_long: body.iss_position.longitude ,iss_lat: body.iss_position.latitude })
-    })
-    .catch(error => console.error(`Error in fetch: ${error.message}`));
 
-  }
 
   render() {
 
@@ -34,7 +16,7 @@ class Basics extends Component {
     return(
       <div>
         <h1>Iss current coordinates</h1>
-        <h3>lat: {this.state.iss_lat} long: {this.state.iss_long}</h3>
+      
       </div>
     )
   }
