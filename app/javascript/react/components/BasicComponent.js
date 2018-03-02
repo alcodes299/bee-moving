3f95e31e124873236b7bc226f46ff00855250ba6
alcodes299 <alskis299@gmail.com>
2018-03-01 21:12:40 -0500

    add some javascript to the main page!

--- app/javascript/react/components/BasicComponent.js
+++ app/javascript/react/components/BasicComponent.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 
 class Basics extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      iss_long: [],
-      iss_last: []
+      quotes: [
+                "Aerodynamically, the bumble bee shouldn't be able to fly, but the bumble bee doesn't know it so it goes on flying anyway. -unknown",
+                "",
+                "",
+                ""
+                ],
+    newQuote: ''
     }
   }
 
 
   render() {
 
-
+    // map quotes into a div
     return(
       <div>
-        <h1>Iss current coordinates</h1>
-      
+        <h1>Nature Quotes,Facts,Questions, Form</h1>
       </div>
     )
   }
